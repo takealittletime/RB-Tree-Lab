b35bb4e46fc5c5f20e2fa0ca2a9b0dbb60272d4d
ChoiMinKyu <chlalsrb118@gmail.com>
2024-10-11 16:40:15 +0000

    RBTree_submit_01

--- src/rbtree.c
+++ src/rbtree.c
@@ -2,35 +2,98 @@
 
 #include <stdlib.h>
 
+//tmp/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+typedef enum { RBTREE_RED, RBTREE_BLACK } color_t;
+
+typedef int key_t;
+
+typedef struct node_t {
+  color_t color;
+  key_t key;
+  struct node_t *parent, *left, *right;
+} node_t;
+
+typedef struct {
+  node_t *root;
+  node_t *nil;  // for sentinel
+} rbtree;
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 rbtree *new_rbtree(void) {
+  // make new rbtree
   rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
-  // TODO: initialize struct if needed
+  // if rbtree is created, initialize its root node and nil node.
+  if (p != NULL)
+  {
+    p->nil = (node_t*)calloc(1,sizeof(rbtree));
+    p->nil->color = RBTREE_BLACK;
+    p->root = p->nil;
+  }
+  // return rbtree
   return p;
 }
 
 void delete_rbtree(rbtree *t) {
-  // TODO: reclaim the tree nodes's memory
+  // reclaim the tree nodes's memory
   free(t);
 }
 
 node_t *rbtree_insert(rbtree *t, const key_t key) {
   // TODO: implement insert
+  node_t tmp;
+  tmp.color = RBTREE_RED;
+  tmp.key = key;
+
+  if (t->root == NULL){
+
+  }
   return t->root;
 }
 
 node_t *rbtree_find(const rbtree *t, const key_t key) {
-  // TODO: implement find
-  return t->root;
+  node_t* tmp = t->root;
+
+  while (tmp != t->nil)
+  {
+    if (tmp->key == key)
+    {
+      return tmp;
+    }
+    else if (tmp->key > key)
+    {
+      tmp = t->root->left;
+    }
+    else
+    {
+      tmp = t->root->right;
+    }
+  }
+  
+  return tmp;
 }
 
 node_t *rbtree_min(const rbtree *t) {
-  // TODO: implement find
-  return t->root;
+  // create tmp and put root node
+  node_t* tmp = t->root;
+  // just go left till meet nill node
+  while (tmp != t->nil)
+  {
+    tmp = tmp->left;
+  }
+
+  return tmp;
 }
 
 node_t *rbtree_max(const rbtree *t) {
-  // TODO: implement find
-  return t->root;
+  // create tmp and put root node
+  node_t* tmp = t->root;
+  // just go right till meet nill node
+  while (tmp != t->nil)
+  {
+    tmp = tmp->right;
+  }
+
+  return tmp;
 }
 
 int rbtree_erase(rbtree *t, node_t *p) {
